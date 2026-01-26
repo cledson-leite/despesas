2a9210282040b5424f0d7f5038d3eefaad0b24bc
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2026-01-26 15:06:43 -0300

    criar modal transaction form

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
-  TransactionForm(this.onSubmit, {super.key});
+  const TransactionForm(this.onSubmit, {super.key});
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final TextEditingController titleController = TextEditingController();
+
+  final TextEditingController valueController = TextEditingController();
 
   void _handleTextField() {
     final title = titleController.text;
@@ -13,7 +19,7 @@ class TransactionForm extends StatelessWidget {
     if(title.isEmpty || value <= 0) {
       return;
     }
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,91 @@
-import 'package:despesas/components/transaction_user.dart';
+import 'dart:math';
+
+import 'package:despesas/components/transaction_form.dart';
+import 'package:despesas/components/transaction_list.dart';
+import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(ExpenseApp());
+void main() => runApp(MaterialApp(home: ExpenseApp()));
 
-class ExpenseApp extends StatelessWidget {
+class ExpenseApp extends StatefulWidget {
   const ExpenseApp({super.key});
 
   @override
+  State<ExpenseApp> createState() => _ExpenseAppState();
+}
+
+class _ExpenseAppState extends State<ExpenseApp> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+  void _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
           centerTitle: true,
           title: Text(
             'Despesas Pessoais',
             style: TextStyle(color: Colors.white, fontSize: 28),
           ),
+          actions: [
+            IconButton(
+              onPressed: () => _openTransactionFormModal(context),
+              icon: Icon(Icons.add),
+              iconSize: 28,
+            ),
+          ],
         ),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Card(child: Text('Gráficos')),
-              TransactionUser(),
+              TransactionList(_transactions),
             ],
           ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _openTransactionFormModal(context),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      );
   }
 }
